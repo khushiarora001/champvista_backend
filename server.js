af39971886da6bc1d9382d94bae1b504f05b1d4e
Khushi <khushi.arora@nestorbird.com>
2025-02-13 16:18:52 +0530

    add porrt 5001

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
 
 module.exports = app;
